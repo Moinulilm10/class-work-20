db87d9ecd6a2898bcd93c529c64e47034a7d812c
Moinul Islam <moinulilm10@gmail.com>
2024-05-28 23:58:15 +0600

    "Updated resume PDF file name from 'Moinul_Islam_Resume.pdf' to 'Moinul_Islam_CV.pdf'"

--- portfolio client/src/components/Resume/Resume.jsx
+++ portfolio client/src/components/Resume/Resume.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../../assets/Moinul_Islam_Resume.pdf";
+import pdf from "../../assets/Moinul_Islam_CV.pdf";
 import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
